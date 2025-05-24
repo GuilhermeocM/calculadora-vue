cc3270a15ee06b0631b57a828c5c41fab6bc7380
Guilherme Mascarenhas <guilhermeoc.m@gmail.com>
2025-05-24 01:29:51 -0300

    secund commit

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,33 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <header class="p-5 pb-2 mb-4 mt-4 bg-light rounded-3">
+      <h1>Calculdora</h1>
+      <p class="text-end">escolha abaixo a operação que deseja realizar <i class="bi bi-arrow-down"></i></p>
+    </header>
+    <form>
+    <div class="row">
+      <div class="col">
+        <input type="number" placeholder="Digite o primeiro número" class="form-control">
+      </div>
+      <div class="col">
+        <input type="number" placeholder="Digite o segundo número" class="form-control">
+      </div>
+      <div class="col">
+        <select class="form-control">
+          <option value="adicao">Adição</option>
+          <option value="subtracao">Subtração</option>
+          <option value="multiplicacao">Multiplicação</option>
+          <option value="divisao">Divisão</option>
+        </select>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </form>
+  </div>
+  
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
