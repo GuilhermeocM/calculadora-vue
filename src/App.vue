a90a6aac63c96b590cca3c26b62f312d4d63b5ac
Guilherme Mascarenhas <guilhermeoc.m@gmail.com>
2025-05-27 00:28:15 -0300

    antes dos componets

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,28 @@
 <script setup>
+import { reactive } from 'vue';
+
+
+const estado = reactive({
+  filtro: 'adicao',
+  primeiroNumero: 0,
+  segundoNumero: 0,
+  resultado: 0,
+})
+
+const imprimeResultado = () => {
+  const { filtro } = estado
+
+  switch (filtro) {
+    case 'adicao':
+      return estado.resultado = estado.primeiroNumero + estado.segundoNumero
+    case 'subtracao':
+      return estado.resultado = estado.primeiroNumero - estado.segundoNumero
+    case 'multiplicacao':
+      return estado.resultado = estado.primeiroNumero * estado.segundoNumero
+    case 'divisao':
+      return estado.resultado = estado.primeiroNumero / estado.segundoNumero
+  }
+}
 
 </script>
 
@@ -11,13 +35,13 @@
     <form>
     <div class="row">
       <div class="col">
-        <input type="number" placeholder="Digite o primeiro número" class="form-control">
+        <input type="number" placeholder="Digite o primeiro número" class="form-control" @keyup="evento => { estado.primeiroNumero = Number(evento.target.value); imprimeResultado()}">
       </div>
       <div class="col">
-        <input type="number" placeholder="Digite o segundo número" class="form-control">
+        <input type="number" placeholder="Digite o segundo número" class="form-control" @keyup="evento => { estado.segundoNumero = Number(evento.target.value); imprimeResultado()}">
       </div>
       <div class="col">
-        <select class="form-control">
+        <select @change="evento => { estado.filtro = evento.target.value; imprimeResultado() }" class="form-control">
           <option value="adicao">Adição</option>
           <option value="subtracao">Subtração</option>
           <option value="multiplicacao">Multiplicação</option>
@@ -26,8 +50,19 @@
       </div>
     </div>
   </form>
+  <p class="resultado">
+    {{ estado.resultado }}
+  </p>
   </div>
   
 </template>
 
-<style scoped></style>
+<style scoped>
+
+.resultado{
+  font-size: 48px;
+  font-weight: bold;
+  margin: 10px;
+}
+
+</style>
